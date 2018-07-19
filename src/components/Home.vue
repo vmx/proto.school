742acd0a3ab43adf94607913034a0f007c6f4d8d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-19 02:03:02 +0000

    Deploy mikeal/learn-ipfs to github.com/mikeal/learn-ipfs.git:gh-pages

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <ul>
+      <li>
+        <router-link to="/01">Lesson 1: Basics</router-link>
+      </li>
+      <li>
+        <router-link to="/02">Lesson 2: TBD</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scopted>
+</style>
